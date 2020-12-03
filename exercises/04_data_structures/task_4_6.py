5ce79670d86733fa06d98a659def8e84fdd9e462
python <python@debian>
2020-12-03 07:19:38 -0600

    1 Решение 4.6

--- exercises/04_data_structures/task_4_6.py
+++ exercises/04_data_structures/task_4_6.py
@@ -14,3 +14,12 @@ Outbound Interface    FastEthernet0/0
 """
 
 ospf_route = "      10.0.24.0/24 [110/41] via 10.0.13.3, 3d18h, FastEthernet0/0"
+out = "\n{:25} {}" * 5
+ospf = ospf_route.replace(",","").replace("[","").replace("]","")
+ospf = ospf.split()
+print(out.format(
+'Prefix',ospf[0],
+'AD/Metric',ospf[1],
+'Next-Hop',ospf[3],
+'Out update',ospf[4],
+'Outbound Interface',ospf[5]))
