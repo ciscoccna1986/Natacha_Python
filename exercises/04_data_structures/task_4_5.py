234830f0e885501fe5da39cd79c7ff5d03a37d47
python <python@debian>
2020-12-03 06:22:29 -0600

    Переделал 4.5

--- exercises/04_data_structures/task_4_5.py
+++ exercises/04_data_structures/task_4_5.py
@@ -16,4 +16,4 @@ command2 = "switchport trunk allowed vlan 1,3,8,9"
 
 set1 = set(command1.split()[-1].split(','))
 set2 = set(command2.split()[-1].split(','))
-print(sorted(list(set1 & set2)))
+print(sorted(set1 & set2))
