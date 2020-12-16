7aabdf6003552752a5be40bb4f8eda700ee8aa42
python <python@debian>
2020-12-16 08:29:51 -0600

    Решение

--- exercises/05_basic_scripts/task_5_3.py
+++ exercises/05_basic_scripts/task_5_3.py
@@ -63,3 +63,13 @@ trunk_template = [
     "switchport mode trunk",
     "switchport trunk allowed vlan {}",
 ]
+
+
+template = {"access" : access_template , "trunk" : trunk_template }
+
+mode = input("Введите режим работы интерфейса (access/trunk): ")
+interface = input("Введите тип и номер интерфейса: ")
+vlan = input("Введите номер влан(ов): ")
+
+print("Interface {}".format(interface))
+print("\n".join(template[mode]).format(vlan))
